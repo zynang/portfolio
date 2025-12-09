3dce34ec79e2643bedc3521ebc8caf1d705332a9
Zyn Yee <zyn_yee_ang@brown.edu>
2025-12-09 13:15:02 +0800

    update

--- js/custom.js
+++ js/custom.js
@@ -285,7 +285,7 @@ document.addEventListener('DOMContentLoaded', () => {
 const dynamicText = document.getElementById('dynamicText');
 
 // Array of words to display
-const words = ['naval officer', 'fan of tea', 'baker of sweet treats', 'friend', 'sister'];
+const words = ['naval officer', 'baker of sweet treats', 'friend', 'sister'];
 let currentWordIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
